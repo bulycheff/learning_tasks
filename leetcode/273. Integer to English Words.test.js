b6d31097efc2a8749122f3a8d7e4148cd3f58f05
Sergey Bulychev <bulycheff@ya.ru>
2021-12-25 01:01:36 +0500

    25.12.2021_00

---
+++ leetcode/273. Integer to English Words.test.js
@@ -0,0 +1,22 @@
+const { numberToWords } = require('./273. Integer to English Words')
+
+describe('273. Integer to English Words', () => {
+
+  test('273. Integer to English Words', () => {
+
+    expect(numberToWords(0)).toBe('Zero')
+    expect(numberToWords(1)).toBe('One')
+    expect(numberToWords(10)).toBe('Ten')
+    expect(numberToWords(15)).toBe('Fifteen')
+    expect(numberToWords(45)).toBe('Forty Five')
+    expect(numberToWords(100)).toBe('One Hundred')
+    expect(numberToWords(109)).toBe('One Hundred Nine')
+    expect(numberToWords(1000)).toBe('One Thousand')
+    expect(numberToWords(1590)).toBe('One Thousand Five Hundred Ninety')
+    expect(numberToWords(10000)).toBe('Ten Thousand')
+    expect(numberToWords(15990)).toBe('Fifteen Thousand Nine Hundred Ninety')
+    expect(numberToWords(1234567)).toBe('One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven')
+
+  })
+
+})
